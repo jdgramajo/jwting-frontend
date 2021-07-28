ec34ae4fa9bedd2887ba23f3777eeb4da177a7e5
JD <jdgramajo@gmail.com>
2021-07-27 20:04:50 -0600

    Implements function binding to find a workaround to form submition and custom function usage.

--- src/controller.js
+++ src/controller.js
@@ -2,6 +2,7 @@ import createHeaderComponent from "./shared/v1/components/header/creator";
 import createFormComponent from "./shared/v1/components/form/creator";
 import router from "./shared/v1/router";
 import { login } from "./shared/v1/services/formsService";
+// import FormService from "./shared/v1/services/formsService";
 
 const header = createHeaderComponent([
   { routerFunction: router.toRoot, name: "Sign in" },
--- src/shared/v1/components/form/creator.js
+++ src/shared/v1/components/form/creator.js
@@ -1,16 +1,6 @@
 import FormModel from "./model";
 
-const createFormComponent = (
-  options = {
-    formId: "john-doe-of-forms",
-    formGroups: [],
-    submitText: "Submit",
-    submitFunctionString: "event.preventDefault(); alert('Form action!');",
-    submitFunction: () => {
-      alert("Form action!");
-    },
-  }
-) => {
+const createFormComponent = (options) => {
   const formComponent = new FormModel(options);
 
   return formComponent;
