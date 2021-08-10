2d1b1e621da510b53eaa25235f9c1498365090e7
JD <jdgramajo@gmail.com>
2021-08-10 01:03:16 -0600

    Moves code around to help transpiler, work in progress.

--- src/v1/main/controller.js
+++ src/v1/main/controller.js
@@ -2,14 +2,47 @@ import createHeaderComponent from "../../shared/v1/components/header/creator";
 import router from "../../shared/v1/router";
 import jwt from "../../../jwt.svg";
 import createMainComponent from "./creator";
+import { getMyRolesRequest } from "../../shared/v1/requests/auth";
 
-document.getElementById("tabIcon").setAttribute("href", jwt);
+const addRolesToMain = async (mainComponent) => {
+  const response = await getMyRolesRequest();
 
-const headerComponent = createHeaderComponent([
-  { routerFunction: router.toChangePwd, name: "Change Password" },
-  { routerFunction: router.toSignout, name: "Sign out" },
-]);
-headerComponent.appendComponentToElement(document.body);
+  if (!response.ok) {
+    // TODO: Appropriate actions based on error types.
+    console.log(response.statusText);
+    router.toError();
+    return;
+  }
 
-const mainComponent = createMainComponent();
-mainComponent.appendComponentToElement(document.body);
+  const { roles } = await response.json();
+
+  if (roles) {
+    mainComponent.mainTitleText.innerText = "Login successful!";
+    mainComponent.rolesTitleText.innerText = "Your roles are:";
+    roles.map((role) => {
+      const item = document.createElement("li");
+      item.innerText = `${role}`;
+      mainComponent.rolesList.appendChild(item);
+    });
+  } else {
+    mainComponent.mainTitleText.innerText = "Login successful...";
+    mainComponent.rolesTitleText.innerText = "...but getting roles failed.";
+    mainComponent.mainTitleText.innerText = `No roles found: ${roles}`;
+  }
+};
+
+const start = async () => {
+  document.getElementById("tabIcon").setAttribute("href", jwt);
+
+  const headerComponent = createHeaderComponent([
+    { routerFunction: router.toChangePwd, name: "Change Password" },
+    { routerFunction: router.toSignout, name: "Sign out" },
+  ]);
+  headerComponent.appendComponentToElement(document.body);
+
+  const mainComponent = createMainComponent();
+  await addRolesToMain(mainComponent);
+  mainComponent.appendComponentToElement(document.body);
+};
+
+start();
--- src/v1/main/creator.js
+++ src/v1/main/creator.js
@@ -1,34 +1,8 @@
 import MainModel from "./model";
-import { getMyRolesRequest } from "../../shared/v1/requests/auth";
 
-const createMainComponent = async () => {
-  const response = await getMyRolesRequest();
-
-  if (!response.ok) {
-    // TODO: Appropriate actions based on error types.
-    console.log(response.statusText);
-    router.toError();
-    return;
-  }
-
-  const { roles } = await response.json();
-  if (roles) {
-    const mainComponent = new MainModel(roles);
-    mainComponent.mainTitleText.innerText = "Login successful!";
-    mainComponent.rolesTitleText.innerText = "Your roles are:";
-    roles.map((role) => {
-      const item = document.createElement("li");
-      item.innerText = `${role}`;
-      mainComponent.rolesList.appendChild(item);
-    });
-    return mainComponent;
-  } else {
-    const mainComponent = new MainModel(roles);
-    mainComponent.mainTitleText.innerText = "Login successful...";
-    mainComponent.rolesTitleText.innerText = "...but getting roles failed.";
-    mainComponent.mainTitleText.innerText = `No roles found: ${roles}`;
-    return mainComponent;
-  }
+const createMainComponent = () => {
+  const mainComponent = new MainModel();
+  return mainComponent;
 };
 
 export default createMainComponent;
